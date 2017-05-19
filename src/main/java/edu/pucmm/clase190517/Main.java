16d51ec0bd8c7dc278838d2656d6e647e2e76ed1
ricardo <20131196@ce.pucmm.edu.do>
2017-05-19 12:30:47 -0400

    hello world

--- src/main/java/edu/pucmm/clase190517/Main.java
+++ src/main/java/edu/pucmm/clase190517/Main.java
@@ -5,6 +5,6 @@ package edu.pucmm.clase190517;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
     }
 }
