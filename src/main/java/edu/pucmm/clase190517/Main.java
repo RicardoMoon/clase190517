f0f13044ef369f6e1f0b032fcd9128e1803f78b4
ricardo <20131196@ce.pucmm.edu.do>
2017-05-19 12:26:31 -0400

    main hello

--- src/main/java/edu/pucmm/clase190517/Main.java
+++ src/main/java/edu/pucmm/clase190517/Main.java
@@ -4,5 +4,7 @@ package edu.pucmm.clase190517;
  * Created by Adolfo on 19/5/2017.
  */
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
